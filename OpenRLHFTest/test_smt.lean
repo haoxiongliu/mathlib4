7e2537c0eb5b25b05f2245af14d7d57fedded99d
liuhx20 <liuhx20@mails.tsinghua.edu.cn>
2025-04-10 00:56:22 +0800

    add Smt.Real

--- OpenRLHFTest/test_smt.lean
+++ OpenRLHFTest/test_smt.lean
@@ -1,4 +1,8 @@
 import Smt
+import Smt.Real
 
 example {p q r : Prop} (hp : ¬p) (hq : ¬q) (hr : r) : ¬(p ∨ q ∨ ¬r) := by
   smt [hp, hq, hr]
+
+example (ε : Real) (h1 : ε > 0) : ε / 2 + ε / 3 + ε / 7 < ε := by
+  smt [h1]
